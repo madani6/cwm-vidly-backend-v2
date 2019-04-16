912bcf2c189276daf13d0d0dbb7166c7b95dbe88
madani6 <anandamadani06@gmail.com>
2019-04-16 10:30:14 +0700

    Populating the Form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,9 +2,16 @@ import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndopoint = apiUrl + "/movies";
+
 export function getMovies() {
   return http.get(apiEndopoint);
 }
+
+export function getMovie(movieId) {
+  return http.get(apiEndopoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndopoint + "/" + movieId);
