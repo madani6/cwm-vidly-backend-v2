a0eb1a84821a2486a3a3f00681ad4a2ce03e4b0d
madani6 <anandamadani06@gmail.com>
2019-04-16 10:45:35 +0700

    Saving the Movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,7 +11,15 @@ export function getMovie(movieId) {
   return http.get(apiEndopoint + "/" + movieId);
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(apiEndopoint + "/" + movie._id, body);
+  }
+
+  return http.post(apiEndopoint, movie);
+}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndopoint + "/" + movieId);
